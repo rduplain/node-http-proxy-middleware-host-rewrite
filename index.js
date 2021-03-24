fede535e46e867118914a99a425b9d672e161d44
Rom DuPlain <rom.duplain@gmail.com>
2021-03-23 18:46:16 -0700

    Add middleware to the middleware to rewrite hosts.

--- index.js
+++ index.js
@@ -13,15 +13,44 @@ app.use(
 )
 
 app.use(
+  '/one',
+  function (_, res) {
+    res.end("Route One")
+  }
+)
+
+app.use(
+  '/two',
+  function (_, res) {
+    res.end("Route Two")
+  }
+)
+
+var middleware = proxy({
+  target: 'http://localtest.me:3000/404',
+  changeOrigin: true,
+  router: {
+    'example.localtest.me:3000': 'http://example.com',
+    'hooli.localtest.me:3000': 'http://hooli.xyz',
+    'one': 'http://localtest.me:3000/one',
+    'two': 'http://localtest.me:3000/two'
+  }
+})
+
+var one = /.*-?one-?.*/
+var two = /.*-?two-?.*/
+
+app.use(
   '/',
-  proxy({
-    target: 'http://localtest.me:3000/404',
-    changeOrigin: true,
-    router: {
-      'example.localtest.me:3000': 'http://example.com',
-      'hooli.localtest.me:3000': 'http://hooli.xyz'
+  function (req, res, next) {
+    // Rewrite host header for `router` lookup when regular expression matches.
+    if (req.headers.host.match(one)) {
+      req.headers.host = 'one'
+    } else if (req.headers.host.match(two)) {
+      req.headers.host = 'two'
     }
-  })
+    return middleware(req, res, next)
+  }
 )
 
 app.listen(3000)
