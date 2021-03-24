a5e7bcbf0a4367f1889e7e704843960528e2629e
Rom DuPlain <rom.duplain@gmail.com>
2021-03-23 18:05:49 -0700

    Add 404 handler.

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ var app = express();
 
 app.use(require('morgan')('dev'))  // Enable access logging.
 
+app.use(
+  '/404',
+  function (_, res) {
+    res.sendStatus(404)
+  }
+)
+
 app.listen(3000)
